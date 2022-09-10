c5fee014889148da0e80f1571d101b9d2c3c97c3
SergeyDolgushin <sergei.dolgushin@mail.ru>
2022-09-10 22:22:51 +0300

    задача 1.3

--- js-features/mole-game/base.js
+++ js-features/mole-game/base.js
@@ -1,3 +1,31 @@
+const holes = document.querySelectorAll('.hole');
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
+let isMole = 0;
+let missing = 0;
+
+
+holes.forEach((hole) => {
+  hole.addEventListener('click', (evt) => {
+    evt.target.classList.contains('hole_has-mole') ? isMole++ : missing++;
+    dead.textContent = isMole;
+    lost.textContent = missing;
+    
+    if (isMole === 10) {
+      isMole = 0; 
+      missing = 0;
+      alert('Вы выиграли');
+    }
+
+    if (missing === 5) {
+      isMole = 0; 
+      missing = 0;
+      alert('Вы проиграли');
+    }
+
+  });
+});
+
 (() => {
   let playing = true,
     activeHole = 1;
