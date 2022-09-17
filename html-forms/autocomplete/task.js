a05c0f83d4a00ce83d90696d650ed57293ed3359
SergeyDolgushin <sergei.dolgushin@mail.ru>
2022-09-17 21:39:53 +0300

    autocomplete ready

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,18 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    const _answer = [];
+    
+    for (const option of this.input.options){
+      if (option.textContent.startsWith(text)) {
+        _answer.push({
+          text: option.textContent,
+          value: option.value  
+        });
+      } 
+    }
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    return _answer
   }
 }
 
